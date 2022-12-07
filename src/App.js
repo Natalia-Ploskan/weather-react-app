7a24eacf547169eef6269dc629b81d434f6f7792
Natalia-Ploskan <ploskannatali@gmail.com>
2022-12-07 22:09:55 +0200

    add one city

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <Weather city="Kyiv" />
+        <Weather city="Lutsk" />
         <a
           className="App-link"
           href="https://reactjs.org"

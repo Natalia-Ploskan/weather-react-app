c8812d182f81aaea900e07737aac4809f961f419
Natalia-Ploskan <ploskannatali@gmail.com>
2022-12-11 11:56:01 +0200

    freaking div

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
@@ -29,17 +29,19 @@ export default function Weather() {
         <li>Friday 12:00</li>
         <li>Light rain</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-            alt="Light Rain"
-          ></img>
-          <span className="temperature">1</span>
-          <span className="unit">°C</span>
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+              alt="Light Rain"
+              className="float-left"
+            />
+            <span className="temperature">1</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
-          {" "}
           <ul>
             <li>Precipitation: 15% </li>
             <li>Humidity: 98%</li>

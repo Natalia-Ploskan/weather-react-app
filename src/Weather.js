6e2cc2823a147b49ad3a762efec99803afe328c7
Natalia-Ploskan <ploskannatali@gmail.com>
2022-12-21 22:42:25 +0200

    hz

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,7 @@ export default function Weather(props) {
     });
   }
   function search() {
-    const apiKey = "3c082189faac84be221ca0ee1f2ba89c";
+    const apiKey = "6f578b96aa9505bcce148ac22cb85794";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }

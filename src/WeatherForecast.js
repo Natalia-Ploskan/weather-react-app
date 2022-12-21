c5617eefb5fcba7f0853a311bfba900697e356a0
Natalia-Ploskan <ploskannatali@gmail.com>
2022-12-21 22:33:26 +0200

    add tricky forecast 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -14,12 +14,17 @@ export default function WeatherForecast(props) {
   if (loaded) {
     return (
       <div className="WeatherForecast">
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <WeatherForecastDay data={forecast[0]} />
-            </div>
-          </div>
+        <div className="row mt-4">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  {" "}
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );

370a61f7466158eccf463b49d5774f3ece07e298
Natalia-Ploskan <ploskannatali@gmail.com>
2022-12-21 22:40:43 +0200

    update apiKey

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -28,7 +28,7 @@ export default function WeatherForecast(props) {
       </div>
     );
   } else {
-    let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
+    let apiKey = "6f578b96aa9505bcce148ac22cb85794";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
